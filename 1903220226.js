7886514738194e80c0b4d1650e62cd813bc82195
mariely2009 <mariely.fernanda.silva@escola.pr.gov.br>
2026-03-27 07:56:48 -0300

    updte

---
+++ 1903220226.js
@@ -0,0 +1,14 @@
+const filaNumber = [1,2,3,4,5,6];
+const filaStringAmigos = ["Ana Julia", "Blob, "Patrick"];
+    function lerAmigos (filaStringAmigos) {
+      console.log( 'Você tem ${filaSringAmigos.length} amigos)
+       
+        for(let i = 0; 1< filaStringAmigos,length;i++){
+           console.log(filaStringAmigos[i])
+           if(filaStringAmigos[i]=== "Bob"){
+            console.log
+           }
+
+        }
+
+    } 
